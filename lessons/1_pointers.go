f778cf4b697388800f98d7b47274197b8a74b17f
Ilja Pavlovs <ilja.pavlovs@gmail.com>
2022-12-01 19:47:52 +0100

    update

--- lessons/1_pointers.go
+++ lessons/1_pointers.go
@@ -19,6 +19,13 @@ func (v *Vertex) Scale(f float64) {
 	fmt.Println("v.Y inside Scale function: ", v.Y)
 }
 
+func (v Vertex) ScaleWithoutPointerReceiver(f float64) {
+	v.X = v.X * f
+	fmt.Println("v.X inside ScaleWithoutPointerReceiver function: ", v.X)
+	v.Y = v.Y * f
+	fmt.Println("v.Y inside ScaleWithoutPointerReceiver function: ", v.Y)
+}
+
 func main() {
 	v := Vertex{3, 4}
 	//TODO - do we need to use & in this case?
@@ -31,6 +38,10 @@ func main() {
 
 	fmt.Println("v.X inside Main after Scale function: ", v.X) // RESULT will be 30, SO IT STRUCT FIELD WAS CHANGED THANKS TO A POINTER
 	fmt.Println("v.Y inside Main after Scale function: ", v.Y) // RESULT will be 40, SO IT STRUCT FIELD WAS CHANGED THANKS TO A POINTER
+
+	v.ScaleWithoutPointerReceiver(10)
+	fmt.Println("v.X inside Main after ScaleWithoutPointerReceiver function: ", v.X) // RESULT will be 3, SO IT STRUCT FIELD WAS NOT CHANGED
+	fmt.Println("v.Y inside Main after ScaleWithoutPointerReceiver function: ", v.Y) // RESULT will be 4, SO IT STRUCT FIELD WAS NOT CHANGED
 
 	//fmt.Println(v.Abs())
 }
